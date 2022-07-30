8dc1f0f46cec5861fb7bd9686a10ee35daf1a707
Kenneth Cluck <klc3rd@tycho.lan>
2022-07-30 08:56:07 -0600

    Corrected method for retrieving user information in a ticket page

--- pages/api/tickets/[tid].ts
+++ pages/api/tickets/[tid].ts
@@ -1,7 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Session, unstable_getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
+import { ISecuredUser } from "user-types";
 import Ticket from "../../../lib/db/schemas/Ticket";
+import User from "../../../lib/db/schemas/Users";
+
 import Err from "../../../lib/Err";
 
 const GetTicket = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -25,7 +28,12 @@ const GetTicket = async (req: NextApiRequest, res: NextApiResponse) => {
         throw new Err(422, "You do not have permission to see this ticket");
       }
 
-      res.status(200).send(ticket);
+      // Get posting user
+      const user: ISecuredUser | null = await User.findOne({
+        _id: ticket?.poster_id,
+      });
+
+      res.status(200).send({ user: user, ticket: ticket });
     } catch (err) {
       /**
        * Determine type of error object before returning error
--- pages/tickets/[tid].tsx
+++ pages/tickets/[tid].tsx
@@ -1,3 +1,8 @@
+/******************************
+ * This page displays the ticket itself as well as responses
+ * this is also where you manage the ticket
+ ******************************/
+
 import { useEffect, useState } from "react";
 import MainContainer from "../../components/main-container";
 import { useRouter } from "next/router";
@@ -13,18 +18,63 @@ interface ITicketPage {
   userRole: string;
 }
 
+interface ITicketData {
+  user: ISecuredUser;
+  ticket: ITicket;
+}
+
 const TicketPage: React.FC<ITicketPage> = (props) => {
   const { userid, username, userRole } = props;
   const [error, setError] = useState<string | null>(null);
-  const [ticket, setTicket] = useState<ITicket | null>(null);
+  const [ticketData, setTicketData] = useState<ITicketData | null>(null);
   const [status, setStatus] = useState<string | null>(null);
   const [date, setDate] = useState<string | null>(null);
-  const [postingUser, setPostingUser] = useState<ISecuredUser | null>(null);
   const [assignableStatus, setAssignableStatus] = useState<string | null>(null);
 
   // Get ticket id from URL Param
   const router = useRouter();
   const ticket_id = router.query.tid;
+
+  /**
+   * Get info for ticket
+   */
+  const getPostData = () => {
+    const assignableCheck = async () => {
+      if (
+        (userRole == "admin" || userRole == "tech") &&
+        ticketData &&
+        ticketData.user.username !== username
+      ) {
+        if (ticketData.ticket.assigned_id !== userid) {
+          setAssignableStatus("assignable");
+        } else {
+          setAssignableStatus("assigned");
+        }
+      }
+    };
+
+    if (ticketData?.ticket) {
+      // Format date
+      setDate(new Date(ticketData!.ticket!.date).toLocaleString());
+
+      // Set status
+      switch (ticketData!.ticket.status) {
+        case Status.Closed:
+          setStatus("Closed");
+          break;
+        case Status.Unassigned:
+          setStatus("Unassigned");
+          break;
+        case Status.AwaitingTechniciansResponse:
+          setStatus("Awaiting Technician Response");
+          break;
+        case Status.AwaitingUserResponse:
+          setStatus("Awaiting User Response");
+          break;
+      }
+    }
+    assignableCheck();
+  };
 
   /**
    *  Retrieve ticket
@@ -39,68 +89,12 @@ const TicketPage: React.FC<ITicketPage> = (props) => {
         return;
       }
 
-      setTicket(data);
+      setTicketData(data);
     };
 
     getTicket();
-  }, [status]);
-
-  /**
-   * Get info for ticket
-   */
-  useEffect(() => {
-    const getPostingUser = async () => {
-      if (ticket) {
-        const response = await fetch(`/api/getuser/${ticket.poster_id}`);
-        const data = await response.json();
-
-        if (response.status !== 200) {
-          setError(data.message);
-          return;
-        }
-
-        setPostingUser(data);
-      }
-    };
-
-    const assignableCheck = async () => {
-      if (
-        (userRole == "admin" || userRole == "tech") &&
-        postingUser &&
-        postingUser.username !== username
-      ) {
-        if (postingUser.assigned_id !== userid) {
-          setAssignableStatus("assignable");
-        } else {
-          setAssignableStatus("assigned");
-        }
-      }
-    };
-
-    if (ticket) {
-      // Format date
-      setDate(new Date(ticket!.date).toLocaleString());
-
-      // Set status
-      switch (ticket.status) {
-        case Status.Closed:
-          setStatus("Closed");
-          break;
-        case Status.Unassigned:
-          setStatus("Unassigned");
-          break;
-        case Status.AwaitingTechniciansResponse:
-          setStatus("Awaiting Technician Response");
-          break;
-        case Status.AwaitingUserResponse:
-          setStatus("Awaiting User Response");
-          break;
-      }
-
-      getPostingUser();
-      assignableCheck();
-    }
-  }, [ticket]);
+    getPostData();
+  }, [status, ticketData]);
 
   /**
    * Return main profile page
@@ -113,7 +107,7 @@ const TicketPage: React.FC<ITicketPage> = (props) => {
           <div className="ticket-grid">
             <div className="ticket-grid-header">Posted By</div>
             <div className="ticket-grid-shortfield">
-              {postingUser?.username}
+              {ticketData?.user.username}
             </div>
             {assignableStatus == "assignable" && (
               <div className="ticket-grid-btnfield">
@@ -126,21 +120,29 @@ const TicketPage: React.FC<ITicketPage> = (props) => {
               </div>
             )}
             <div className="ticket-grid-header">Ticket Number</div>
-            <div className="ticket-grid-shortfield">{ticket?.number}</div>
+            <div className="ticket-grid-shortfield">
+              {ticketData?.ticket.number}
+            </div>
             <div className="ticket-grid-header">Date</div>
             <div className="ticket-grid-shortfield">{date}</div>
             <div className="ticket-grid-header">Device</div>
-            <div className="ticket-grid-shortfield">{ticket?.device}</div>
+            <div className="ticket-grid-shortfield">
+              {ticketData?.ticket.device}
+            </div>
             <div className="ticket-grid-header">Summary</div>
-            <div className="ticket-grid-field">{ticket?.summary}</div>
+            <div className="ticket-grid-field">
+              {ticketData?.ticket.summary}
+            </div>
             <div className="ticket-grid-header">Status</div>
             <div className="ticket-grid-shortfield">{status}</div>
             <div className="ticket-grid-header">Issue</div>
-            <div className="ticket-grid-field">{ticket?.issue}</div>
-            {ticket?.steps && (
+            <div className="ticket-grid-field">{ticketData?.ticket.issue}</div>
+            {ticketData?.ticket.steps && (
               <>
                 <div className="ticket-grid-header">Steps</div>
-                <div className="ticket-grid-field">{ticket?.steps}</div>
+                <div className="ticket-grid-field">
+                  {ticketData?.ticket.steps}
+                </div>
               </>
             )}
           </div>
