376f7c6cbcf9e658a0113c6a4db9e727433a3496
Kenneth Cluck <klc3rd@tycho.lan>
2022-08-03 21:06:03 -0600

    Added final code other than demo mode and minor changes

--- components/ticket-list/mainpanel.tsx
+++ components/ticket-list/mainpanel.tsx
@@ -1,7 +1,7 @@
 /**
  * This panel shows the stats on the main page with links to filters
  */
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Button from "../ui/button";
 import Status from "../../lib/enums/ticket-status";
 import { ITicket } from "ticket-types";
@@ -25,6 +25,8 @@ const MainPanel: React.FC<IMainPanel> = (props) => {
   const [counts, setCounts] = useState<ITicketCounts | null>(null);
   const [error, setError] = useState<string | null>(null);
 
+  const selectRef = useRef<HTMLSelectElement | null>(null);
+
   useEffect(() => {
     const getCounts = async () => {
       const response = await fetch("/api/tickets/get/counts");
@@ -41,6 +43,13 @@ const MainPanel: React.FC<IMainPanel> = (props) => {
     getCounts();
   }, []);
 
+  const changeStatusMenu = () => {
+    const statusVal = selectRef.current?.value;
+    const newStatus: Status = parseInt(statusVal!);
+
+    onFilterChange(newStatus);
+  };
+
   return (
     <>
       <div className="main-panel">
@@ -52,53 +61,80 @@ const MainPanel: React.FC<IMainPanel> = (props) => {
         <div className="main-panel-container">
           {error && <span className="error">{error}</span>}
           {!error && (
-            <div>
-              <span
-                className="main-panel-link"
-                onClick={() => {
-                  onFilterChange(Status.All);
-                }}
-              >
-                Total - {counts?.totalTickets}
-              </span>
-              |
-              <span
-                className="main-panel-link"
-                onClick={() => {
-                  onFilterChange(Status.Open);
-                }}
-              >
-                Open - {counts?.openTickets}
-              </span>
-              |
-              <span
-                className="main-panel-link"
-                onClick={() => {
-                  onFilterChange(Status.AwaitingUserResponse);
-                }}
-              >
-                Awaiting User Response - {counts?.awaitingUsersResponse}
-              </span>
-              |
-              <span
-                className="main-panel-link"
-                onClick={() => {
-                  onFilterChange(Status.AwaitingTechniciansResponse);
-                }}
-              >
-                Awaiting Technician Response -{" "}
-                {counts?.awaitingTechniciansResponse}
-              </span>
-              |
-              <span
-                className="main-panel-link"
-                onClick={() => {
-                  onFilterChange(Status.Closed);
-                }}
-              >
-                Closed - {counts?.closedTickets}
-              </span>
-            </div>
+            <>
+              <div className="main-panel-minibox">
+                <select
+                  className="main-panel-select"
+                  name="status"
+                  onChange={changeStatusMenu}
+                  ref={selectRef}
+                >
+                  <option value={Status.All}>
+                    Total Tickets - {counts?.totalTickets}
+                  </option>
+                  <option value={Status.Open}>
+                    Open Tickets - {counts?.openTickets}
+                  </option>
+                  <option value={Status.AwaitingTechniciansResponse}>
+                    Awaiting Technician Response -{" "}
+                    {counts?.awaitingTechniciansResponse}
+                  </option>
+                  <option value={Status.AwaitingUserResponse}>
+                    Awaiting User Response - {counts?.awaitingUsersResponse}
+                  </option>
+                  <option value={Status.Closed}>
+                    Closed Tickets - {counts?.closedTickets}
+                  </option>
+                </select>
+              </div>
+              <div className="main-panel-box">
+                <span
+                  className="main-panel-link"
+                  onClick={() => {
+                    onFilterChange(Status.All);
+                  }}
+                >
+                  Total - {counts?.totalTickets}
+                </span>
+                |
+                <span
+                  className="main-panel-link"
+                  onClick={() => {
+                    onFilterChange(Status.Open);
+                  }}
+                >
+                  Open - {counts?.openTickets}
+                </span>
+                |
+                <span
+                  className="main-panel-link"
+                  onClick={() => {
+                    onFilterChange(Status.AwaitingUserResponse);
+                  }}
+                >
+                  Awaiting User Response - {counts?.awaitingUsersResponse}
+                </span>
+                |
+                <span
+                  className="main-panel-link"
+                  onClick={() => {
+                    onFilterChange(Status.AwaitingTechniciansResponse);
+                  }}
+                >
+                  Awaiting Technician Response -{" "}
+                  {counts?.awaitingTechniciansResponse}
+                </span>
+                |
+                <span
+                  className="main-panel-link"
+                  onClick={() => {
+                    onFilterChange(Status.Closed);
+                  }}
+                >
+                  Closed - {counts?.closedTickets}
+                </span>
+              </div>
+            </>
           )}
         </div>
       </div>
